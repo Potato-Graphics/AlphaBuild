b783cabdeac014570b0ace983b6c6d02fe616111
Harry Williams <hw223759@falmouth.ac.uk>
2019-11-12 09:46:46 +0000

    Project moved to HDR pipeline

---
+++ Assets/Scripts 1/Weapon.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public Rigidbody bulletPrefab;
+    public float speed = 20;
+    public bool canShoot = true;
+    public bool delay = false;
+
+    public bool rotation = false;
+
+    public Player player;
+
+    WaitForSeconds cooldownTime;
+
+
+    void Start()
+    {
+        cooldownTime = new WaitForSeconds(0.1f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Fire1") > 0.0f)
+        {
+            canShoot = true;
+            Shoot(true);
+        }
+
+        if (Input.GetAxis("Fire1") <= 0.0f)
+        {
+            canShoot = false;
+            Shoot(false);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //ShootDiagonally();
+        }
+    }
+
+    void Shoot(bool Shoot)
+    {
+        /*
+        while (!delay)
+        {
+            canShoot = Shoot;
+            if (!canShoot || delay)
+                return;
+            Rigidbody test = Instantiate<Rigidbody>(bulletPrefab, firePoint.position, firePoint.rotation);
+            Vector3 direction = player.rotation ? transform.up * (speed * Time.deltaTime) * transform.localScale.z : transform.right * (speed * Time.deltaTime) * transform.localScale.x;
+            test.velocity = transform.TransformDirection(direction);
+            ShootUpwards();
+            delay = true;
+            StartCoroutine(ShootPause());
+        }
+        */
+
+        if (!delay)
+        {
+            canShoot = Shoot;
+            if (!canShoot || delay)
+                return;
+            Rigidbody test = Instantiate<Rigidbody>(bulletPrefab, firePoint.position, firePoint.rotation);
+            Vector3 direction = player.rotation ? transform.up * (speed * Time.deltaTime) * transform.localScale.z : transform.right * (speed * Time.deltaTime) * transform.localScale.x;
+            test.transform.TransformDirection(direction);
+            test.velocity = transform.TransformDirection(direction);
+            ShootUpwards();
+            delay = true;
+            StartCoroutine(ShootPause());
+        }
+    }
+
+    void ShootUpwards()
+    {
+        rotation = !rotation;
+    }
+
+   /* void ShootDiagonally()
+    {
+            Rigidbody thisBullet =  Instantiate(bulletPrefab, firePoint.position, firePoint.rotation) as Rigidbody;
+        //  Vector3 newRotation = transform.eulerAngles;
+        //newRotation.x = 
+        //thisBullet.transform.eulerAngles = new Vector3(0, 45, 0);
+        Vector3 rotation = new Vector3(0, 45, 0);
+        Quaternion rotationAngle = Quaternion.Euler(rotation);
+        Vector3 velocity = rotationAngle * (speed * Time.deltaTime) * transform.localScale.x;
+        
+        thisBullet.velocity = 
+           // thisBullet.transform.rotation = newRotation;
+            
+    }
+    */
+    IEnumerator ShootPause()
+    {
+        yield return cooldownTime;
+        delay = false;
+    }
+}
