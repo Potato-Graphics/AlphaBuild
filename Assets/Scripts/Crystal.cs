ad117802898cb30d4d99a2fdbc80387bea378803
Oliver <ot218255@falmouth.ac.uk>
2019-12-03 16:12:48 +0000

    Added a new castle prefab and changed some things.

--- Assets/Scripts/Crystal.cs
+++ Assets/Scripts/Crystal.cs
@@ -12,36 +12,33 @@ public class Crystal : MonoBehaviour
     public GameObject implosionPrefab;
     public GameObject sparksPrefab;
 
-    public AudioSource source;
-    public AudioClip explosionSound;
-    public AudioClip bomb;
-
     Player target;
     Vector2 moveDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        target = GameObject.FindObjectOfType<Player>();
-        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
-        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        target = GameObject.FindObjectOfType<Player>(); //Finds the player.
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed; // Moves Towards the player.
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y); // The speed in which the crystal will move.
         Destroy(gameObject, 3f);
     }
 
     // Update is called once per frame
     void OnCollisionEnter(Collision col)
     {
+
+        // Spawns the implosion effect when the crystal hits an object.
         if (col.gameObject.tag == "Player" || col.gameObject.tag.Contains("Obstacles"))
         {
             Instantiate(implosionPrefab, gameObject.transform.position, Quaternion.identity);
-            source.PlayOneShot(explosionSound);
             Destroy(gameObject);
         }
-
+        
+        // Spawns a seperate effect for when the player shoots at the crystal.
         else if (col.gameObject.tag == "Bullet")
         {
             Instantiate(sparksPrefab, gameObject.transform.position, Quaternion.identity);
-            source.PlayOneShot(bomb);
             Destroy(gameObject);
         }
     }
