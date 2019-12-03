f513af285b3b4954063cbb55099065fa8b1285f9
CallumAddy <ca222335@falmouth.ac.uk>
2019-12-03 13:54:42 +0000

    Added sounds, projectiles an music.

---
+++ Assets/Scripts/Crystal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float moveSpeed = 10f;
+
+    Rigidbody rb;
+
+    public GameObject implosionPrefab;
+    public GameObject sparksPrefab;
+
+    public AudioSource source;
+    public AudioClip explosionSound;
+    public AudioClip bomb;
+
+    Player target;
+    Vector2 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        target = GameObject.FindObjectOfType<Player>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag.Contains("Obstacles"))
+        {
+            Instantiate(implosionPrefab, gameObject.transform.position, Quaternion.identity);
+            source.PlayOneShot(explosionSound);
+            Destroy(gameObject);
+        }
+
+        else if (col.gameObject.tag == "Bullet")
+        {
+            Instantiate(sparksPrefab, gameObject.transform.position, Quaternion.identity);
+            source.PlayOneShot(bomb);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UISounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISounds : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip hover;
+    public AudioClip click;
+
+
+    // Start is called before the first frame update
+    public void OnHover()
+    {
+        source.PlayOneShot(hover);
+    }
+
+    public void Onclick()
+    {
+        source.PlayOneShot(click);
+    }
+}
