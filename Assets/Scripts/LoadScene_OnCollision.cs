67f6275a9af9f95c42c8ce3aa0d647a21f5d543b
CallumAddy <ca222335@falmouth.ac.uk>
2020-01-10 14:34:47 +0000

    Demo day tweaks

---
+++ Assets/Scripts/LoadScene_OnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene_OnCollision : MonoBehaviour
+{
+    [SerializeField] int sceneID;
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        print("test");
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneID);
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        print("test jd");
+        print(col.gameObject.tag);
+        if (col.gameObject.tag == "Player")
+        {
+            print("gfd");
+            SceneManager.LoadScene(sceneID);
+        }
+    }
+}
