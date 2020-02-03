78a941e41e644c2aa8786a33c68793eb55086f33
Harry Williams <hw223759@falmouth.ac.uk>
2020-02-03 16:40:42 +0000

    Started on special attack UI slider. Code for player filled with errors. Not to sure what happened between friday and today. Ollie/Callum could you please look into this.

---
+++ Assets/AmmoIcon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class AmmoIcon : MonoBehaviour
+{
+    public Slider SuperAttack;
+    public bool isFiring;
+    public int superSoakerAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SuperAttack.Slider = superSoakerAmmo.ToString();
+        if (Input.GetMouseButtonDown(0) && !isFiring && superSoakerAmmo > 0)
+        {
+            isFiring = true;
+            superSoakerAmmo--;
+            isFiring = false;
+        }
+    }
+}
---
+++ Assets/SuperSoaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperSoaker : MonoBehaviour
+{
+
+    private Transform bar; 
+    
+    // Start is called before the first frame update
+    private void start()
+    {
+        bar = transform.Find("bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+}
