1630a4c1a7c4cb9b213bd85f19ec53a8b43e3451
uhazgotme <hw223759@falmouth.ac.uk>
2020-02-25 14:33:35 +0000

    VFX FOR DIRECTIONAL SHOOTING

--- Assets/Scripts/Player Scripts/Weapon.cs
+++ Assets/Scripts/Player Scripts/Weapon.cs
@@ -11,7 +11,11 @@ public class Weapon : MonoBehaviour
 
     public float fireRate = 0;
     public LayerMask notToHit;
-    public Rigidbody2D bulletPrefab;
+    public Rigidbody2D bulletUp;
+    public Rigidbody2D bulletDown;
+    public Rigidbody2D bulletHorizontal;
+    public Rigidbody2D bulletDiagDown;
+    public Rigidbody2D bulletDiagUp;
     public Vector3 position;
     [SerializeField] float speed = 25;
 
@@ -49,11 +53,12 @@ public class Weapon : MonoBehaviour
         if (direction.y > 350 && direction.y < 600 && direction.x > 450 && direction.x < 542)
         {
             //this is up
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D bullet = Instantiate(bulletUp, firePoint.position, firePoint.rotation);
             //BulletScript.yDirection = 0.1f;
             //BulletScript.xDirection = 0.0f;
             Vector2 velocityChange = new Vector2(0.0f, 0.1f);
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
+            print("up");
             anim.SetTrigger("FireUp");
         }
      
@@ -61,70 +66,80 @@ public class Weapon : MonoBehaviour
         {
             //this is down
             anim.SetTrigger("FireDown");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D bullet = Instantiate(bulletDown, firePoint.position, firePoint.rotation);
             //BulletScript.yDirection = -0.1f;
             // BulletScript.xDirection = 0.0f;
             Vector2 velocityChange = new Vector2(0, -0.1f);
+            print("down");
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
         else if (direction.x > 600 && direction.x < 1000 && direction.y < 600 && direction.y > 370)
         {
             //this is right up
             anim.SetTrigger("FireDiagUp");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.localRotation);
+            Rigidbody2D bullet = Instantiate(bulletDiagUp, firePoint.position, firePoint.localRotation);
             //BulletScript.xDirection = 0.1f;
             // BulletScript.yDirection = 0.1f;
             Vector2 velocityChange = new Vector2(0.1f, 0.1f);
+            print("rignht up");
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
         else if (direction.x > 525 && direction.x < 1000 && direction.y > 270 && direction.y < 370)
         {
             //this is right
             anim.SetTrigger("FireHorizontal");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.localRotation);
+            Rigidbody2D bullet = Instantiate(bulletHorizontal, firePoint.position, firePoint.localRotation);
             // BulletScript.xDirection = 0.1f;
             // BulletScript.yDirection = 0.0f;
             Vector2 velocityChange = new Vector2(0.1f, 0);
+            print("right");
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
         else if (direction.x > 450 && direction.x < 1000 && direction.y < 270 && direction.y > 0)
         {
             //this is right down
             anim.SetTrigger("FireDiagDown");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.localRotation);
+            Rigidbody2D bullet = Instantiate(bulletDiagDown, firePoint.position, firePoint.localRotation);
             //BulletScript.xDirection = 0.1f;
             //BulletScript.yDirection = -0.1f;
             Vector2 velocityChange = new Vector2(0.1f, -0.1f);
+            print("right down");
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
         else if (direction.x > 0 && direction.x < 450 && direction.y < 600 && direction.y > 370)
         {
             //this is left up
             anim.SetTrigger("FireDiagUp");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.localRotation);
+            Rigidbody2D bullet = Instantiate(bulletDiagUp, firePoint.position, firePoint.localRotation);
             //BulletScript.xDirection = -0.1f;
             // BulletScript.yDirection = 0.1f;
             Vector2 velocityChange = new Vector2(-0.1f, 0.1f);
+            bullet.transform.eulerAngles = new Vector2(0, -180);
+            print("left up");
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
         else if (direction.x > 0 && direction.x < 525 && direction.y > 270 && direction.y < 370)
         {
             //this is left
             anim.SetTrigger("FireHorizontal");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.localRotation);
+            Rigidbody2D bullet = Instantiate(bulletHorizontal, firePoint.position, firePoint.localRotation);
             //BulletScript.xDirection = -0.1f;
             // BulletScript.yDirection = 0.0f;
             Vector2 velocityChange = new Vector2(-0.1f, 0);
+            bullet.transform.eulerAngles = new Vector2(0, -180);
+            print("left");
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
         else if (direction.x > 0 && direction.x < 450 && direction.y < 270 && direction.y > 0)
         {
             //this is left down
             anim.SetTrigger("FireDiagDown");
-            Rigidbody2D bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.localRotation);
+            Rigidbody2D bullet = Instantiate(bulletDiagDown, firePoint.position, firePoint.localRotation);
             // BulletScript.xDirection = -0.1f;
             //  BulletScript.yDirection = -0.1f;
             Vector2 velocityChange = new Vector2(-0.1f, -0.1f);
+            print("left down");
+            bullet.transform.eulerAngles = new Vector2(0, -180);
             bullet.velocity = velocityChange * (Time.deltaTime * speed);
         }
 
