1ad2b86e7f99e638cd70afaa10adb8a9bbbb5976
CallumAddy <ca222335@falmouth.ac.uk>
2020-04-30 04:34:44 +0100

    Life & coin colelctible changes

---
+++ Assets/CollectLife.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollectLife : MonoBehaviour
+{
+    Player player;
+    void Start()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if(player.GetHealth() < 3)
+            {
+                player.UpdateHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
